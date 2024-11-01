59bc20237c781c610f64d27eeb3f49444151ecef
Leonardo R. Duarte <105319590+Duarte097@users.noreply.github.com>
2024-11-01 17:20:04 -0300

    Initial commit of Angular and backend projects

---
+++ login-auth-api/src/main/java/com/example/login_auth_api/Infra/Security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.login_auth_api.Infra.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.example.login_auth_api.domain.user.User;
+import com.example.login_auth_api.repositories.UserRepository;
+import java.util.Collections;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+      User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));  
+      return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), Collections.emptyList());
+    } 
+}
